f44a9542ddc4e17448891bc54ed1f3175d2353b5
mukeshdidel <mukeshdidel11111@gmail.com>
2026-05-17 20:11:11 +0530

    vertex buffer layout

--- MyGameEngine/src/platform/OpenGL/OpenGLBuffer.h
+++ MyGameEngine/src/platform/OpenGL/OpenGLBuffer.h
@@ -9,8 +9,13 @@ namespace soul {
 		virtual ~OpenGLVertexBuffer();
 		virtual void Bind() const override;
 		virtual void Unbind() const override;
+
+		virtual void SetLayout(const BufferLayout& layout) override { m_Layout = layout; }
+		virtual const BufferLayout& GetLayout() const override { return m_Layout; }
+
 	private:
 		uint32_t m_RendererID;
+		BufferLayout m_Layout;
 	};
 
 	class OpenGLIndexBuffer : public IndexBuffer {
@@ -19,6 +24,8 @@ namespace soul {
 		virtual ~OpenGLIndexBuffer();
 		virtual void Bind() const override;
 		virtual void Unbind() const override;
+
+		virtual void SetLayout(const BufferLayout& layout) override {}
 
 		virtual uint32_t GetCount() const override { return m_Count; }
 
